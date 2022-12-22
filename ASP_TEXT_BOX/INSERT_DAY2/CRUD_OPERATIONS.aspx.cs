e0830bfb66a7ef0fde82b23949be8019dcf9219f
Roopeshkrishna143 <roopeshkrishna20@gmail.com>
2022-12-22 17:59:30 +0530

    vffsdcc

--- ASP_TEXT_BOX/INSERT_DAY2/CRUD_OPERATIONS.aspx.cs
+++ ASP_TEXT_BOX/INSERT_DAY2/CRUD_OPERATIONS.aspx.cs
@@ -17,7 +17,7 @@ namespace ASP_TEXT_BOX.INSERT_DAY2
 
         protected void Button1_Click(object sender, EventArgs e)
         {
-            SqlConnection con = new  SqlConnection  SqlConnection(@"Data Source=LAPTOP-QDBEGBSE\SQLEXPRESS;Initial Catalog=Roopesh_Test;Integrated Security=True");
+            SqlConnection con = new  SqlConnection (@"Data Source=LAPTOP-QDBEGBSE\SQLEXPRESS;Initial Catalog=Roopesh_Test;Integrated Security=True");
             con.Open();
             SqlCommand cmm = new SqlCommand("insert into [dbo].[Patient_Info_ROOPEE] values('"+int.Parse(TextBox1.Text)+"','"+TextBox2.Text+"','"+TextBox3.Text+"','"+TextBox4.Text+"','"+DropDownList1.SelectedValue+ "','"+TextBox6.Text+"')",con);
             cmm.ExecuteNonQuery();
